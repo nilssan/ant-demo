5249211fd321f8065d90f39634672e7389721b83
Nilesh Pawar <pawarn987@gmail.com>
2020-06-24 09:15:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+// import { Button, DatePicker, message } from 'antd';
+import { DatePicker, message, Alert } from 'antd';
+
+import './App.css';
+
+function App() {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        {/* Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'} */}
+        <Alert message="Selected Date" description={date ? date.format('YYYY-MM-DD') : 'None'} />
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
